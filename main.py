967ba3d0ac2c6eb7a04d9aaee1ccb5555da1a441
Hitarth Shah <hitarth@hitarths-air.lan>
2023-12-26 00:39:45 -0800

    Model Training and Evaluation Completed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from MLOPS_Project import logger
 from MLOPS_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLOPS_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLOPS_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLOPS_Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from MLOPS_Project.pipeline.stage_05_model_evaluatino import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -29,7 +31,27 @@ try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     obj = DataTransformationTrainingPipeline()
     obj.main()
-    logger.info(f">>>> stage {STAGE_NAME} completed <<<<")
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx*****x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx******x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = ModelEvaluationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx******x")
 except Exception as e:
     logger.exception(e)
     raise e
